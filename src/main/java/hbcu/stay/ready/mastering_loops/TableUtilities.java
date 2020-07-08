8e7c4e50e74c99b97728ac79dac6c98a7064ac56
Jubril Aderomilehin <Jubril@Jubrils-MacBook-Air.local>
2020-07-08 17:41:24 -0400

    Updated work

--- src/main/java/hbcu/stay/ready/mastering_loops/TableUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TableUtilities.java
@@ -1,15 +1,50 @@
 package hbcu.stay.ready.mastering_loops;
 
 public class TableUtilities {
+    //method header, returns string, accepts nothing.., basing output on test case
     public static String getSmallMultiplicationTable() {
-        return null;
+        //init empty string 
+        String getSmall = "";
+        //loop for 5 times inclusively starting at 1
+        for( int i = 1; i <= 5; i++){
+            //for every loop we're adding a new row
+            int length = 5;
+            getSmall += buildTableRow(i,length)+ "\n";
+
+        }
+        return getSmall;
     }
 
+
+
+    private static String buildTableRow(int i, int length){
+        String rowBuild = "";
+        for(int x = 1; x <= length; x++){
+            int number = x * i;
+            rowBuild += String.format("%3d |", number);
+        }
+        return rowBuild;
+    } 
+    
+
     public static String getLargeMultiplicationTable() {
-        return null;
+        String getLarge = "";
+        for( int i = 1; i <= 10; i++){
+            int length = 10;
+            getLarge += buildTableRow(i,length)+ "\n";
+        }
+
+        return getLarge;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String getMultiplication = "";
+        for( int i = 1; i <= 20; i++){
+            int length = 20;
+            getMultiplication += buildTableRow(i,length)+ "\n";
+        }
+        
+        return getMultiplication;
     }
 }
+
--- src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
@@ -1,20 +1,45 @@
 package hbcu.stay.ready.mastering_loops;
 
 public class TriangleUtilities {
+    /*The first two functions work together to build a triangle 
+    - first we get the num rows, then the number of stars */
+
+    //spits out a string with the number of stars we input
+    // ex: getTriangle(20) -> "********************" (20 star string)
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        //init empty output string
+        String empty = "";
+        //Loop the number of times we want, starting at 1 to be inclusive.
+        for( int i = 1; i < numberOfRows; i++){
+            //adds row full of stars to output str and adds new line
+            empty += getRow(i) + "\n";
+        }
+        //return output string
+        return empty;
     }
 
+    //getRow(20) -> "********************"
     public static String getRow(int numberOfStars) {
-        return null;
+        //init empty 
+        String empty = "";
+        //Loop the number of times we want, starting at 1 and using "<=" to be inclusive.
+        //Start  | Condition for loop to run | Step
+        for(int i = 1; i <= numberOfStars; i++){
+            //add star to output str
+            empty += "*";
+        }
+        //return string
+        return empty;
     }
-
+    // test case says 4 rows, if your  get triangle method works
+    // you should be able to return getTriangle(4)
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
-
+    // test case says 9 rows, if your  get triangle method works
+    // you should be able to return getTriangle(9)
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
